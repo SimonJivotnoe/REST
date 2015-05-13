db14af19248c5f9c238f87a6cf9a35ecd987a2e5
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-13 12:55:23 +0300

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -3,7 +3,6 @@ function myFunc()
 {
 	$.ajax({
 		type: "GET",
-		//url: 'http://localhost/rest/server/api/car/Hello_world_action/.json',
 		url: 'api/autos',
 		cache: false,                                 
 		success: function(response){
