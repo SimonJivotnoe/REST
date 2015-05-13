2c840b341500c0d8b967970c1332b2ff51a96bc2
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-13 13:47:24 +0300

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,16 +1,28 @@
 
-function myFunc()
+function listOfAutos()
 {
 	$.ajax({
 		type: "GET",
 		url: '/~user1/PHP/rest/client/api/autos/',
 		cache: false,                                 
 		success: function(response){
-		alert(response);
+		console.log(response);
+		}
+	});
+}
+function details()
+{
+	$.ajax({
+		type: "GET",
+		url: '/~user1/PHP/rest/client/api/autos/1',
+		cache: false,                                 
+		success: function(response){
+		console.log(response);
 		}
 	});
 }
 $( document ).ready( function ()
 {
-    myFunc();
+    listOfAutos();
+    details();
 })
