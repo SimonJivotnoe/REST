bc5973da4366ffe6cfa333f4b6e8822fa965de35
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-13 14:26:34 +0300

    Update ajaxQueries.js

--- client/js/ajaxQueries.js
+++ client/js/ajaxQueries.js
@@ -1,1 +1,22 @@
-
+function listOfAutos()
+{
+	$.ajax({
+		type: "GET",
+		url: '/~user1/PHP/rest/client/api/autos/',
+		cache: false,                                 
+		success: function(response){
+		console.log(response);
+		}
+	});
+}
+function details()
+{
+	$.ajax({
+		type: "GET",
+		url: '/~user1/PHP/rest/client/api/autos/1',
+		cache: false,                                 
+		success: function(response){
+		console.log(response);
+		}
+	});
+}
