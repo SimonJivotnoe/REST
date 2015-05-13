7cc1d6bae24520f79a3b4effcb01f71cb0cbbab1
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-13 16:13:22 +0300

    Update ajaxQueries.js

--- client/js/ajaxQueries.js
+++ client/js/ajaxQueries.js
@@ -38,7 +38,7 @@ function details(id)
         url   : '/~user1/PHP/rest/client/api/autos/' + id,
         method: 'GET'
     } ).then( function ( data )
-    {
+    {console.log(data);
         var objJSON = JSON.parse( data );
         if (objJSON.length > 0) {
         $('.content' ).html('<div class="col-md-4 col-md-offset-1 well">' +
