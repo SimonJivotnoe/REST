fb47c00810c2ac42139a936cefe9359ab73b4a78
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-13 16:05:33 +0300

    Update ajaxQueries.js

--- client/js/ajaxQueries.js
+++ client/js/ajaxQueries.js
@@ -1,4 +1,4 @@
-function listOfAutos()
+/*function listOfAutos()
 {
 	$.ajax({
 		type: "GET",
@@ -8,6 +8,29 @@ function listOfAutos()
 		console.log(response);
 		}
 	});
+}*/
+function listOfAutos()
+{
+    $.ajax( {
+        url   : '/~user1/PHP/rest/client/api/autos/',
+        method: 'GET'
+    } ).then( function ( data )
+    {
+        var objJSON = JSON.parse( data );
+        $.each(objJSON, function(key, val){
+            $.each(val, function(key, val){
+                if ('id' == key) {
+                    $('.content' ).append(
+                        '<div class="col-md-4 col-md-offset-1 well">' +
+                        '<div class="product" name="'+val+'"></div>' +
+                        '<button class="btn btn-danger order" type="button" name="'+val+'">Order</button></div>');
+                } else {
+                    $('.content').children().last().find('.product').append('<h2>'+val+'</h2>');
+                }
+            })
+        })
+        $('body').fadeIn(50);
+    } )
 }
 function details()
 {
