b819d86f0bcc210a42d882e0f6d842d6f9951200
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-14 11:14:32 +0300

    Update app.php

--- server/app.php
+++ server/app.php
@@ -11,42 +11,44 @@ $app->get('/', function () use ($app) {
     echo $app['view']->render('index');
 });
 
-// Получение всех роботов
 $app->get('/api/autos', function() use ($app) {
-
     $phql = "SELECT * FROM Autos ORDER BY brand";
-    $robots = $app->modelsManager->executeQuery($phql);
-
+    $autos = $app->modelsManager->executeQuery($phql);
     $data = array();
-    foreach( $robots as $robot){
+    foreach( $autos as $auto){
         $data[] = array(
-            'id' => $robot->getId(),
-            'brand' => $robot->getBrand()
+            'id' => $auto->getId(),
+            'img' => $auto->getImg(),
+            'brand' => $auto->getBrand(),
+            'model' => $auto->getModel()
         );
     }
-
     echo json_encode($data);
 });
-
 // Получение робота по ключу
 $app->get('/api/autos/{id:[0-9]+}', function($id) use ($app){
-
     $phql = "SELECT * FROM Autos WHERE id = :id:";
-    $robot = $app->modelsManager->executeQuery($phql, array(
+    $auto = $app->modelsManager->executeQuery($phql, array(
         'id' => $id
     ))->getFirst();
-
     //Create a response
     $response = new Phalcon\Http\Response();
-    if ($robot == false) {
+    if ($auto == false) {
         $response->setJsonContent(array());
     } else {
         $response->setJsonContent(array(
-                'id' => $robot->getId(),
-                'name' => $robot->getBrand()
+            'id' => $auto->getId(),
+            'img' => $auto->getImg(),
+            'brand' => $auto->getBrand(),
+            'model' => $auto->getModel(),
+            'year' => $auto->getModel(),
+            'capacity' => $auto->getModel(),
+            'color' => $auto->getModel(),
+            'max_speed' => $auto->getModel(),
+            'price' => $auto->getModel()
         ));
     }
-    
+
     return $response;
     //return $response;
 });
@@ -57,4 +59,4 @@ $app->notFound(function () use ($app) {
     $app->response->setStatusCode(404, "Not Found")->sendHeaders();
     echo $app['view']->render('404');
 });
-//phalcon create-model --name=autos --output=models --get-set --namespace=\Phalcon\Mvc\Model --extends=Model --doc;
+//phalcon create-model --name=autos --output=models --get-set --namespace=\Phalcon\Mvc\Model --extends=\Phalcon\Mvc\Model --doc;
