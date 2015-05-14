22053a19dbd324c5fdc40d54b3c2e40ffefd5663
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-14 15:48:49 +0300

    Update Formats.php

--- server/models/Formats.php
+++ server/models/Formats.php
@@ -3,12 +3,31 @@
 class Formats {
 
 private $format;
+private $data = array();
 public function __construct ($format) {
   $this->format = $format;
 }
-
+public function transfer ($data) {
+  if ('' == $this->format || '.json' == $this->format) {
+    return $this->returnJSON($data); 
+  } else if ('.xml' == $this->format) {
+    return $this->returnXML($data); 
+  } else if ('.html' == $this->format) {
+    return $this->returnHTML($data); 
+  } else {
+    return $this->returnTXT($data); 
+  }
+}
 public function returnJSON() {
-  
+  foreach( $autos as $auto){
+        $this->data[] = array(
+            'id' => $auto->getId(),
+            'img' => $auto->getImg(),
+            'brand' => $auto->getBrand(),
+            'model' => $auto->getModel()
+        );
+    }
+    return $this->data;
 }
 public function returnXML() {
   
