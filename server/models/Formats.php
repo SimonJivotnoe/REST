a07ed5325c4b1434c4f421450a851061bea91b0b
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-14 15:36:51 +0300

    Update Formats.php

--- server/models/Formats.php
+++ server/models/Formats.php
@@ -2,5 +2,21 @@
 
 class Formats {
 
+private $format;
+public function __construct ($format) {
+  $this->format = $format;
+}
 
+public function returnJSON() {
+  
+}
+public function returnXML() {
+  
+}
+public function returnHTML() {
+  
+}
+public function returnTXT() {
+  
+}
 }
