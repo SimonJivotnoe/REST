db3ed66761f2d5e2e55342456b75760deb234a5a
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-14 11:12:56 +0300

    Update ajaxQueries.js

--- client/js/ajaxQueries.js
+++ client/js/ajaxQueries.js
@@ -1,10 +1,11 @@
 function listOfAutos()
 {
     $.ajax( {
-        url   : '/~user1/PHP/rest/client/api/autos/',
+       // url   : '/~user1/PHP/rest/client/api/autos/',
+        url   : '/client/api/autos/',
         method: 'GET'
     } ).then( function ( data )
-    {console.log(data);
+    {
         var objJSON = JSON.parse( data );
         $.each(objJSON, function(key, val){
             $.each(val, function(key, val){
@@ -12,7 +13,11 @@ function listOfAutos()
                     $('.content' ).append(
                         '<div class="col-md-4 col-md-offset-1 well">' +
                         '<div class="product" name="'+val+'"></div>' +
-                        '<button class="btn btn-danger order" type="button" name="'+val+'">Order</button></div>');
+                        '<button class="btn btn-danger order" disabled type="button" name="'+val+'">Order</button></div>');
+                } else if('img' == key){
+                    $('.content').children().last().find('.product').append(
+                        '<img src="img/' + val + '" width="250" height="200">'
+                    );
                 } else {
                     $('.content').children().last().find('.product').append('<h2>'+val+'</h2>');
                 }
@@ -25,25 +30,29 @@ function listOfAutos()
 function details(id)
 {
     $.ajax( {
-        url   : '/~user1/PHP/rest/client/api/autos/' + id,
+       // url   : '/~user1/PHP/rest/client/api/autos/' + id,
+        url   : '/client/api/autos/' + id,
         method: 'GET'
     } ).then( function ( data )
     {
         var objJSON = JSON.parse( data );console.log($(objJSON).size());//array('status' => 'NOT-FOUND')
         if ($(objJSON).size()> 0) {
-        $('.content' ).html('<div class="col-md-4 col-md-offset-1 well">' +
-        '<div class="details"></div>' +
-        '<div><button class="btn btn-danger order" type="button">Order</button></div>' +
-        '</div>');
+            $('.content' ).html('<div class="col-md-7 col-md-offset-1 well">' +
+            '<div class="details"></div>' +
+            '<div><button class="btn btn-danger order" disabled type="button">Order</button></div>' +
+            '</div>');
             $.each(objJSON, function(key, val){
-                          if ('id' == key) {
-                        $('.order').attr('name', val)
-                    } else {
-                        $('.details' ).append('<p>' + key + ': ' + val + '</p>');
-                    }
+                if ('id' == key) {
+                    $('.order').attr('name', val)
+                } else if('img' == key) {
+                    $('.details' ).append('<img src="img/' + val + '" width="500" height="400">');
+                } else {
+                    $('.details' ).append('<p>' + key + ': ' + val + '</p>');
+                }
             })
         } else {
 
         }
     } )
 }
+
