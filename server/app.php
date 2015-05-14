03d783e3d8ad55732fa3bd4fc9c80167565060a8
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-14 16:00:18 +0300

    Update app.php

--- server/app.php
+++ server/app.php
@@ -32,7 +32,7 @@ $app->get('/api/autos/{string}', function($string) use ($app){
     $objFormat = new Formats($format);
     if ('' == $id) {
         $phql = "SELECT * FROM Autos ORDER BY brand";
-        $autos = $app->modelsManager->executeQuery($phql);
+        $autos = $app->modelsManager->executeQuery($phql);var_dump($autos);
         //$data = array();
     /*foreach( $autos as $auto){
         $data[] = array(
