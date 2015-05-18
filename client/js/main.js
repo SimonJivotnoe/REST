11b00523ec3089c2666c2e6ea0603713c621ac7c
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-18 13:50:39 +0300

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,12 @@
 $( document ).ready( function ()
 {
+    if (null != localStorage.getItem('hash')) {
+        $('.loginStart' ).html('<div class="col-md-6">Welcome</div>' +
+        '<div class="col-md-3">' +
+        '<span class="glyphicon glyphicon-share" aria-hidden="true"></span></div>' +
+        '<div class="col-md-3"><span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span>' +
+        '</div>');
+    }
     var typeId = '.' + $('.active input').attr('id').toLowerCase();
     listOfAutos( typeId );
     $('body').on('click', '.product', function(){
@@ -36,15 +43,15 @@ $( document ).ready( function ()
     });
 
     $('.baner').on('click', function(){
-        window.location.href = "http://192.168.0.15/~user1/PHP/rest/client/";
-        //window.location.href = "http://rest/client/";
+       // window.location.href = "http://192.168.0.15/~user1/PHP/rest/client/";
+        window.location.href = "http://rest/client/";
     })
 
     $('.searchBtn').on('click', function(){
         var searchInput = $('.searchInput').val();
         var searchOption = $('#selectSearch option:selected' ).text();
         if (searchInput.length > 0) {
-            // search(searchInput, searchOption);
+             search(searchInput, searchOption);
         } else {
 
         }
@@ -53,4 +60,60 @@ $( document ).ready( function ()
         var typeId = '.' + $(this ).find('input').attr('id').toLowerCase();
         listOfAutos( typeId );
     })
+
+    $('.glyphicon-user').on('click', function(){
+        registration();
+    })
+
+    $('body').on('click', '#regSubmit', function(){
+        registrationPost($( '#regPassword' ).val(), $( '#regEmail' ).val());
+    })
+
+    $('#loginBtn').on('click', function(){
+        var email = $( '#loginEmail' ).val();
+        var pass = $( '#loginPassword' ).val();
+        var dataForRequest = {"email": email, "pass": pass};
+        logIN( dataForRequest );
+    })
+
+    $('.glyphicon-share').on('click', function(){
+        logOut();
+    })
+    
+    $('.glyphicon-briefcase').on('click', function(){
+        cabinet();
+    })
 })
+
+function registration() {
+    $('.content' ).html('<div class="container well col-md-4 col-md-offset-3">' +
+    '<form method="POST" action="">' +
+    '<div class="form-group">' +
+    '<span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span>' +
+    '<label for="exampleInputPassword1">USER PASSWORD</label><span></span>' +
+    '<input type="password" name="passReg" class="form-control" id="regPassword"' +
+    'placeholder="Password">' +
+    '<p>*min 4 max 10 symbols</p>' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>' +
+    '<label for="exampleInputEmail1">EMAIL ADDRESS</label><span class="err"></span>' +
+    '<input type="email" name="mailReg" class="form-control" id="regEmail"' +
+    ' placeholder="Enter email"></div>' +
+    '<button type="button" id="regSubmit" class="btn btn-success" disabled>Submit</button>' +
+    '</form>' +
+    '</div>');
+}
+
+function logOut() {
+    localStorage.clear();
+    window.location.href = window.location.href;
+}
+
+function orderBtnAction() {
+    if (null != localStorage.getItem('hash')) {
+        $('.order' ).removeAttr( "disabled" );
+    } else {
+        $('.order' ).remove();
+    }
+}
