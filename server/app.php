1ee186e4422c6577eb7ab8a57cc7aa04b743bbb1
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-18 16:10:36 +0300

    Update app.php

--- server/app.php
+++ server/app.php
@@ -145,7 +145,7 @@ $app->put('/api/autos/login', function() use ($app) {
     }
 });
 
-$app->delete('/api/autos/delete/{token}', function($token) use ($app) {
+$app->put('/api/autos/delete/{token}', function($token) use ($app) {
     $phql = "SELECT * FROM UsersRest WHERE token = '$token'";
     $userId = $app->modelsManager->executeQuery($phql)->getFirst();
     if (count($userId)) {
