55d33f95237f478d10f8d20a744b8a1a1a96068f
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-19 16:06:01 +0300

    Update Formats.php

--- server/models/Formats.php
+++ server/models/Formats.php
@@ -23,10 +23,8 @@ class Formats {
             $response = new Phalcon\Http\Response();
             $response->setHeader("Content-type", "application/xml");
             $arr = $this->returnJSON($data);
-            print_r($arr);
-            die();
             $obj = new Array2XML();
-            $xml = $obj->convert($arr);
+            $xml = $obj->convert($data);
             echo $xml;
             die();
             $response->setContent($xml);
